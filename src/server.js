59b6459275921f21b356dd301392d1e881ef0ffb
unknown <arongrhyz@gmail.com>
2022-03-13 15:23:21 +0800

    implemented add todo functionality

--- src/server.js
+++ src/server.js
@@ -1,17 +1,40 @@
 const { ApolloServer, gql } = require("apollo-server");
+const { v4 } = require("uuid");
+
+const todoList = [];
 
 const typeDefs = gql`
+  type Todo {
+    id: String!
+    todo: String!
+  }
+
   type Query {
-    hello: String!
+    getTodo: [Todo!]!
+  }
+
+  type Mutation {
+    addTodo(todo: String!): [Todo!]!
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "Hello worlddd",
+    getTodo: () => todoList,
+  },
+  Mutation: {
+    addTodo: (parents, args, context, info) => {
+      const { todo } = args;
+      const createdTodo = {
+        id: v4(),
+        todo,
+      };
+      todoList.push(createdTodo);
+      return todoList;
+    },
   },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => console.log(url));
+server.listen({ port: 3000 }).then((data) => console.log(data));
